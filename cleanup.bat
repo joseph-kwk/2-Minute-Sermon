6fb4f9bad28eadc630adf08c56a9f98b82b6f2e5
Joseph Kasongo <joseph-kwk@users.noreply.github.com>
2026-08-25 00:16:10 -0500

    chore: clean project directory, keeping only production files

--- cleanup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-del push.bat
-git add -A
-git commit -m "chore: remove temp push script"
-git push
--- gp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git add -A
-git commit -m "feat: YouTube redirect, cursor pointers, red logo title, admin moved to footer"
-git push
